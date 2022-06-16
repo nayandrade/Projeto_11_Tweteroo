691d22dfd757e5e0d17362d8d668fd7991b7c60a
Nayara <nrdeandrade@gmail.com>
2022-06-16 17:07:30 -0300

    feat: post and get method within tweets and sign-up routes

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
 const twiterooServer = express();
 twiterooServer.use(cors());
+twiterooServer.use(express.json());
 
 const users = [
     {
 	username: 'bobesponja', 
 	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
     }
-]
+];
 
-const tweteroo = [
+const tweets = [
     {
-    username: "bobesponja",
-    tweet: "eu amo o hub",
-    }
-]
+	username: "bobesponja",
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	tweet: "eu amo o hub"
+	}
+];
 
+twiterooServer.post('/sign-up', (request, response) => {    
+    users.push(request.body);
+    response.send(users);
+})
+
+twiterooServer.get('/tweets', (request, response) => {
+    response.send(tweets);
+})
+
+twiterooServer.post('/tweets', (request, response) => {
+    const userTweet = {};
+    userTweet.username = request.body.username;
+    userTweet.avatar = users.find(user => user.username === request.body.username).avatar;
+    userTweet.tweet = request.body.tweet;
+    tweets.push(userTweet);
+    response.send(tweets);
+})
 
 
 twiterooServer.listen(5000);
